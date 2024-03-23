c9fd15cfbd5395514ba7c16722a3b15d19dc1628
benli <benli@LAPTOP-ERR69PGH>
2024-03-23 22:05:36 +0100

    completed part 5 of the tutorial

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models;
 
@@ -6,6 +7,8 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
