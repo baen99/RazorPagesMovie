e84eda84c2a7844f0165aba9da82419c6dd93a43
benli <benli@LAPTOP-ERR69PGH>
2024-03-26 22:36:46 +0100

    Enforced Validation Rules onto Movie Attributes (halfway through part 8)

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,14 +6,26 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+
+    [StringLength(60, MinimumLength =3)]
+    [Required]
+    public string Title { get; set; }
 
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
-   /* public decimal Price { get; set; } */
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(50)]
+    public string Genre { get; set; }
+    /* public decimal Price { get; set; } */
+
+    [RegularExpression(@"[A-Z][a-z]*\s[A-Z][a-z]*")]
     public string? Director { get; set; }
+
+    [RegularExpression(@"^[0-9]*/10$")]
+    [StringLength(5)]
     public string Rating { get; set; } = string.Empty;
 
 }
